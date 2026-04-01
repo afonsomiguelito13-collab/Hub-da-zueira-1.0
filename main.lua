3221a8ee6d770db0f3ff417fd3f402e7c5737fa2
afonsomiguelito13-collab <afonsomiguelito13@gmail.com>
2026-04-01 16:56:12 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+local base = "https://raw.githubusercontent.com/afonsomiguelito13-collab/Hub-da-zueira-1.0/main/"
+loadstring(game:HttpGet(base.."parte1.lua"))()
+task.wait(1)
+loadstring(game:HttpGet(base.."parte2.lua"))()
